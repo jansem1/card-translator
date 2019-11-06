a65a5e3117b4cb6a3b89edeb253e4bc54e125be6
Amos Raphenya <raphenar@mcmaster.ca>
2019-11-06 10:43:05 -0500

    revert to older version of galaxy as some amr++ tools won't work in galaxy 19.05

--- docker_image/run.sh
+++ docker_image/run.sh
@@ -3,4 +3,4 @@ sudo docker run --privileged -i -t -p 8989:80 -p 8029:22 -p 9009:9002 \
 -e "GALAXY_CONFIG_ADMIN_USERS=raphenar@mcmaster.ca,mcarthua@mcmaster.ca,jansem1@mcmaster.ca" \
 -e "GALAXY_CONFIG_MASTER_API_KEY=w5CTPRGz6kqtW9BAAAfu3w42Xy" \
 -e "GALAXY_CONFIG_BRAND=marcel_card_megares" -v /home/raphenar/galaxy_docker_images/marcel_card_megares/docker_image/galaxy_storage/:/export/ \
-bgruening/galaxy-stable@sha256:827e648d336503eedf32faaba1ff8d61b12c11449f63dd6610492f5a1d1ebe75
+bgruening/galaxy-stable@sha256:1988374ea86b76405c6011da79a10cbf610d9c4f7ea8aad84c7a39db94190e9b
