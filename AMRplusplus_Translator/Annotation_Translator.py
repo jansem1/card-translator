a64e2f6edc1d80f5b7e91aa2cab2eff34c259136
MJansen2 <jansem1@mcmaster.ca>
2019-12-16 12:54:33 -0500

    Moved to MEGARes 2.0 data, updated code to match

--- AMRplusplus_Translator/Annotation_Translator.py
+++ AMRplusplus_Translator/Annotation_Translator.py
@@ -5,7 +5,7 @@ from datetime import date
 
 # import annotation data
 AroAnn = pd.read_csv('aro_categories_index.tsv', sep='\t', index_col="DNA Accession") # When reading, tsv files must have their delimiter stated
-MegaAnn = pd.read_csv('megares_annotations_v1.01.csv')
+MegaAnn = pd.read_csv('megares_annotations_v2.00.csv') # MEGARes Annotation
 
 # print(AroAnn)
 # print(MegaAnn)
@@ -15,8 +15,8 @@ MegaAnn = pd.read_csv('megares_annotations_v1.01.csv')
 # Create new table containing AMR++-relevant data
 AroCols = [2,3,1] # Important columns from ARO data.
 # ARO's branches are in a different order than MEGARes, hence 2,3,1.
-# Add DNA accession as junk data to fill in header temporarily
-NewAro = AroAnn[AroAnn.columns[AroCols]]
+# Add DNA accession (second column) as junk data to fill in header temporarily
+NewAro = AroAnn[AroAnn.columns[AroCols]] # Creates a Dataframe containing the Columns from the ARO annotation file
 
 # rename columns to match with those of AMR++
 NewAro.index.names = ['header']
@@ -27,4 +27,4 @@ print(NewAro)
 today = date.today()
 filename = ("CARD_to_AMRplusplus_Annotation_" + today.strftime("%Y_%b_%d") + ".csv")
 print(filename)
-# pd.DataFrame.to_csv(NewAro,filename)
+# pd.DataFrame.to_csv(NewAro,filename) # exports converted annotation file as csv
