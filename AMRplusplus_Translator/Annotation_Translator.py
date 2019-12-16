c6792ab15b503580bc3ab34e45f824e1fa288a2a
MJansen2 <jansem1@mcmaster.ca>
2019-12-16 14:24:26 -0500

    Fiddling with indexing

--- AMRplusplus_Translator/Annotation_Translator.py
+++ AMRplusplus_Translator/Annotation_Translator.py
@@ -9,6 +9,8 @@ MegaAnn = pd.read_csv('megares_modified_annotations_v2.00.csv') # MEGARes Annota
 
 # print(AroAnn)
 # print(MegaAnn)
+#TODO:Is the fact that CARD sorts its drugs by semicolon separation in a single cell an issue?
+#TODO: Add type column and find a way to label ARO terms accordingly (Drug, multi-compound resistant, biocide, metal). Just label all as Drugs?
 #TODO: import CARD database (nucleotide_fasta_...), extract the header, add CMG into header
 #TODO: Add this header to final conversion file as index column
 #TODO: Convert AAC(2') and other such groups to -PRIME notation
@@ -17,15 +19,20 @@ MegaAnn = pd.read_csv('megares_modified_annotations_v2.00.csv') # MEGARes Annota
 AroCols = [2,3,1] # Important columns from ARO data.
 # ARO's branches are in a different order than MEGARes, hence 2,3,1.
 # Add DNA accession (second column) as junk data to fill in header temporarily
-NewAro = AroAnn[AroAnn.columns[AroCols]] # Creates a Dataframe containing the Columns from the ARO annotation file
+NewAnn = AroAnn[AroAnn.columns[AroCols]] # Creates a Dataframe containing the Columns from the ARO annotation file
 
 # rename columns to match with those of AMR++
-NewAro.index.names = ['header']
-NewAro.columns = ['class', 'mechanism', 'group']
+NewAnn.index.names = ['header']
+NewAnn.columns = ['class', 'mechanism', 'group']
 
-# Exports AMR++-ready annotation file
-print(NewAro)
+for i in range (0,len(NewAnn.index)):
+    print(NewAnn.iloc[i,0])
+#     print(NewAnn.index[i])
+
+# Exports AMR++-ready annotation file and names it based on the present date
+# print(NewAnn) # Displays table to verify that the translation is occurring properly
 today = date.today()
 filename = ("CARD_to_AMRplusplus_Annotation_" + today.strftime("%Y_%b_%d") + ".csv")
-print(filename)
-# pd.DataFrame.to_csv(NewAro,filename) # exports converted annotation file as csv
+# print(filename)
+
+# pd.DataFrame.to_csv(NewAnn,filename) # exports converted annotation file as csv
--- AMRplusplus_Translator/Database_translator.py
+++ AMRplusplus_Translator/Database_translator.py
@@ -1,4 +1,4 @@
 # Translates CARD database's header into a header that AMR++ can read
 
-#TODO: import CARD database (nucleotide_fasta_...), extract the header, add CMG into header
+#TODO: import CARD database (nucleotide_fasta_...), extract the header, conver to CMG header
 #TODO: Add this header to final conversion file as index column
