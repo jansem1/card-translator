9d424842f5de727304a956e5147d7b161c1e43d7
MJansen2 <jansem1@mcmaster.ca>
2020-01-07 13:03:23 -0500

    Removed need for multiple dataframes, cleaned up comments and formatting

--- AMRplusplus_Translator/Annotation_Translator.py
+++ AMRplusplus_Translator/Annotation_Translator.py
@@ -1,38 +1,60 @@
 # Translates CARD Annotation file into one that AMR++ can read
 
+# TODO: Need to label for multi-compound-resistant determinants for multidrug resistance,
+#  or only for multiple types (eg. Drugs and metals)?
+# TODO: Is the fact that CARD sorts its drugs by semicolon separation in a single cell an issue?
+#  WILL IT CAUSE FALSE NEGATIVES? eg. AMR++ doesn't list an item as carbapenem resistant if it is
+#  labelled "cephalosporin;penam"?
+# TODO: Add type column and find a way to label ARO terms accordingly (Drug, multi-compound resistant, biocide, metal).
+#  Just label all as Drugs?
+# TODO: import CARD database (nucleotide_fasta_...), extract the header, replace with new headers. Will need to find
+#  each header via DNA accession and replace it
+# TODO: Convert AAC(2') and other such groups to -PRIME notation
+#  - Find and delete all parentheses in each cell of the "group" column (BEFORE it is concatenated into the header)
+#   - for open parens, make sure it excludes open parens with a space in front of them (Eg. see line 808 of Jan 6 export)
+#   - For close parens, make sure it's only at the end of the string
+#  - Find all ' in the group column. Replace with '-PRIME' string. Use {Dataframe}.replace
+
+
 import numpy as np
 import pandas as pd
 from datetime import date
 
 # import annotation data
-AroAnn = pd.read_csv('aro_categories_index.tsv', sep='\t') # When reading, tsv files must have their delimiter stated
+AroAnn = pd.read_csv('aro_categories_index.tsv', sep='\t')  # When reading, tsv files must have their delimiter stated
 # MegaAnn = pd.read_csv('megares_modified_annotations_v2.00.csv') # MEGARes Annotation
 
-typeCol = ['Drugs'] * len(AroAnn.index)
-typeAnn = pd.DataFrame(typeCol, columns=['type'])
+typeCol = ['Drugs'] * len(AroAnn.index)  # Creates a list of the string 'Drugs' with as many values as the
+# annotation file has. MEGARes has a type column, but ARO (mostly) only deals with drugs
+typeAnn = pd.DataFrame(typeCol, columns=['type'])  # Turns that list into a Dataframe
 # print(typeAnn)
 
-#TODO: Need to label for multi-compound-resistant determinants for multidrug resistance, or only for multiple types (eg. Drugs and metals)?
-#TODO: Is the fact that CARD sorts its drugs by semicolon separation in a single cell an issue? - WILL IT CAUSE FALSE NEGATIVES? eg. AMR++ doesn't list an item as carbapenem resistant if it is labelled "cephalosporn;penam"?
-#TODO: Add type column and find a way to label ARO terms accordingly (Drug, multi-compound resistant, biocide, metal). Just label all as Drugs?
-#TODO: import CARD database (nucleotide_fasta_...), extract the header, replace with new headers. Will need to find each header via DNA accession and replace it
-#TODO: Convert AAC(2') and other such groups to -PRIME notation
-
 # Create new table containing AMR++-relevant data
-AroCols = [1,3,4,2] # Important columns from ARO data.
-newAnn = AroAnn[AroAnn.columns[AroCols]] # Creates a Dataframe containing the Columns from the ARO annotation file
-# ARO's columns (branches) are in a different order than MEGARes, hence 1,3,4,2.
+AroCols = [1, 3, 4, 2]  # Important columns from ARO data.
+newAnn = AroAnn[AroAnn.columns[AroCols]].copy()  # Creates a Dataframe containing the Columns from the ARO annotation file
+# ARO's columns (branches) are in a different order than MEGARes, so this reorders them.
 # Also Add DNA accession as junk data to fill in header temporarily - fills the same spot as Meg_#
 
-newAnn.columns = ['DNA Accession', 'class', 'mechanism', 'group'] # rename columns to match with those of AMR++
+newAnn.columns = ['DNA Accession', 'class', 'mechanism', 'group']
+#
+# # Removes parentheses in gene family and converts ' to -PRIME. eg. AAC(6') = AAC6-PRIME
+# newAnn['group'] = newAnn['group'].map(str).replace('(')
+# newAnn['group'] = newAnn['group'].map(str).replace('\')','-PRIME')  # will only replace ' when it is next to a close paren,
+# # to prevent accidental removal of apostrophes in areas where they may not be used to indicate PRIME
+# newAnn['group'] = newAnn['group'].map(str).replace('/)$/')
 
-appendCol = newAnn['DNA Accession'].map(str) + "|" + typeAnn['type'].map(str) + "|" + newAnn['class'].map(str) + "|" + newAnn['mechanism'].map(str) + "|" + newAnn['group'].map(str) # concatenate columns to make header
-appColumns = [1,2,3] # Sets the columns from newAnn to be copied over to the final CSV
-finalAnn = pd.concat([appendCol, typeAnn, newAnn[newAnn.columns[appColumns]]], axis=1)
-finalAnn.columns = ['header', 'type', 'class', 'mechanism', 'group']
-print(finalAnn)
 
-today = date.today()
-filename = ("CARD_to_AMRplusplus_Annotation_" + today.strftime("%Y_%b_%d") + ".csv") # Exports AMR++-ready annotation file and names it based on the present date
+appendCol = newAnn['DNA Accession'].map(str) + "|" + typeAnn['type'].map(str) + "|" + newAnn['class'].map(str) + "|" + \
+            newAnn['mechanism'].map(str) + "|" + newAnn['group'].map(str)  # concatenate columns to make header by
+# mapping them to strings  and putting a | between each term
+appColumns = [1, 2, 3]  # Sets the columns from newAnn to be copied over to the final CSV
+newAnn = pd.concat([appendCol, typeAnn, newAnn[newAnn.columns[appColumns]]], axis=1)
+newAnn.columns = ['header', 'type', 'class', 'mechanism', 'group']  # rename columns to match with those of AMR++
 
-pd.DataFrame.to_csv(finalAnn,filename, index=False) # exports converted annotation file as csv
+print(newAnn)
+
+today = date.today()  # get current date
+filename = ("CARD_to_AMRplusplus_Annotation_" + today.strftime("%Y_%b_%d") + ".csv")
+# Exports AMR++-ready annotation file and names it based on the present date
+
+pd.DataFrame.to_csv(newAnn,filename, index=False)  # exports converted annotation file as csv
