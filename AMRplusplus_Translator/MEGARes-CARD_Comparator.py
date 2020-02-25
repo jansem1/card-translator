905d8be246f5f42e32de663e8fd53ab2ce5257de
MJansen2 <jansem1@mcmaster.ca>
2020-02-25 16:23:16 -0500

    find overlapping families/groups using nested for loops. Currently too slow.

--- AMRplusplus_Translator/MEGARes-CARD_Comparator.py
+++ AMRplusplus_Translator/MEGARes-CARD_Comparator.py
@@ -4,9 +4,10 @@
 # Dataframe: [Sequence, CARD gene fam, MEGARes group]
 # - Figure out where the families and groups overlap - bar charts
 
-# TODO: Switch to translated CARD database? That's what's actually being compared
+# TODO: Switch to translated CARD database? That's what's supposedly being compared
 
 import pandas as pd
+import numpy as np
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 #//region Define functions
@@ -42,21 +43,6 @@ def get_groups (data, grouploc, species=False):  # Extracts group from header
     return out
 
 
-def prime_notation(data):  # Converts from (#') to #-PRIME
-    # Removes parentheses in gene family and converts ' to -PRIME and '' to -DPRIME. eg. AAC(6') = AAC6-PRIME
-    data['group'] = data['group'].str.replace('(?<! )\(', '')
-    # replaces open paren only when within a word
-    data['group'] = data['group'].str.replace('\'\'\)$', '-DPRIME')
-    data['group'] = data['group'].str.replace('\'\)$', '-PRIME')  #
-    # Replace ' or '' when next to a close paren and at the end of a line (single gene family)
-    data['group'] = data['group'].str.replace('\'\'\);', '-DPRIME;')
-    data['group'] = data['group'].str.replace('\'\);', '-PRIME;')
-    # Replace ' or '' when next to a close paren and next to a semicolon (multiple gene families)
-    data['group'] = data['group'].str.replace('(?<=\d)\)$', '')
-    data['group'] = data['group'].str.replace('(?<=\d)\);', ';')
-    # replaces ) when preceded by a number and (at the end of a string or semicolon-separated)
-    return data
-
 #//endregion
 
 
@@ -72,6 +58,32 @@ cardData = get_groups(cardData, 5, species=True)
 megData = get_groups(megData, 4)
 # Columns are now 0: Header, 1: Sequence, 2: group
 
-cardData = prime_notation(cardData)
-pd.set_option('display.max_rows', None, 'display.max_columns', None)
-print(cardData['header'].loc[2507])
+# Create column for corresponding group/family
+cardData['in group'] = ""
+megData['in family'] = ""
+# Columns are now 0: Header, 1: Sequence, 2: group, 3: in {bin}
+
+x = 0
+for i in cardData.index:
+    if cardData['sequence'].loc[i] in megData['sequence'].tolist():
+        x+=1
+
+cardInMeg = (str(round(100 * x/len(cardData.index), 2)))
+megInCard = (str(round(100 * x/len(megData.index), 2)))
+print("Percent of CARD homolog model sequences in MEGARes: " + cardInMeg + "%")
+print("Percent of MEGARes sequences in CARD homolog model: " + megInCard + "%")
+
+#//region Find all group/family matches
+# cardData['in group'] = np.where(cardData['sequence'] == megData['sequence'], 'True', 'False')
+# megData['in family'] = np.where(cardData['sequence'] == megData['sequence'], 'True', 'False')
+
+for i in megData.index:
+    print(i)
+    for n in cardData.index:
+        if cardData['sequence'].loc[n] == megData['sequence'].loc[i]:
+            cardData['in group'].loc[n] = megData['group'].loc[i]
+            megData['in family'].loc[i] = cardData['group'].loc[n]
+# TODO: Find a way to do this with generators. This is way too slow.
+# //endregion
+
+print(cardData)
